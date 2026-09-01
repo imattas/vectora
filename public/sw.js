9f6798d0f176d39b34e0b384a73a79079aa1578d
Ian mattas <ian@mattas.net>
2026-08-31 22:08:27 -0400

    feat: add offline mobile app shell

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'vectora-shell-v1';
+const SHELL = ['/', '/help/', '/manifest.webmanifest', '/icon.svg', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).catch(() => caches.match('/')));
+    return;
+  }
+  event.respondWith(fetch(event.request).then(response => {
+    if (response.ok && response.type === 'basic') { const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); }
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
